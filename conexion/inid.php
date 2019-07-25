ce870d676f9c516f628c235ca3fcf94966e8fc7a
GaviotaAntonia <lunita_hermoz@hotmail.com>
2019-07-25 12:41:24 -0500

    nuevos cambios

---
+++ conexion/inid.php
@@ -0,0 +1,40 @@
+<?php 
+
+	$conexion=mysqli_connect('localhost','root','','proyectovidayamor');
+
+ ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>mostrar datos</title>
+</head>
+<body>
+
+<br>
+
+	<table border="1" >
+		<tr>
+			<td>id</td>
+			<td>nombre</td>	
+		</tr>
+
+		<?php 
+		$sql="SELECT * from tipousuario";
+		$result=mysqli_query($conexion,$sql);
+
+		while($mostrar=mysqli_fetch_array($result)){
+		 ?>
+
+		<tr>
+			<td><?php echo $mostrar['id_tipousuario'] ?></td>
+			<td><?php echo $mostrar['tipousuario'] ?></td>
+		</tr>
+	<?php 
+	}
+	 ?>
+	</table>
+
+</body>
+</html>
