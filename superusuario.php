33e946a801930f722602d6f3aeeb2f7ad8253e2c
Natividad Rodriguez <ropy187@outlook.com>
2019-07-26 17:38:23 -0500

    nuevos cambios

--- superusuario.php
+++ superusuario.php
@@ -48,6 +48,55 @@
 }
 </style>
 
+<style >
+  #popup {
+    visibility: hidden;
+    opacity: 0;
+    margin-top: 0px;
+  }
+  #popup:target {
+    visibility:visible;
+    opacity: 1;
+    background-color: rgba(0,0,0,0.8);
+    position: fixed;
+    top:0;
+    left:0;
+    right:0;
+    bottom:0;
+    margin:0;
+    z-index: 000;
+    -webkit-transition:all 1s;
+    -moz-transition:all 1s;
+    transition:all 1s;
+  }
+  .popup-contenedor {
+    position: relative;
+    margin:7% auto;
+    padding:30px 50px;
+    background-color: #fafafa;
+    color:#333;
+    border-radius: 3px;
+    width:50%;
+  }
+  a.popup-cerrar {
+    position: absolute;
+    top:3px;
+    right:3px;
+    background-color: #333;
+    padding:7px 10px;
+    font-size: 20px;
+    text-decoration: none;
+    line-height: 1;
+    color:#fff;
+  }
+
+  a.popup-link {
+      text-align: justify;
+  }
+</style>
+</head>
+<body>
+
 </head>
 <body>
 
@@ -62,19 +111,31 @@
 </div>
 
 <div class="vertical-menu">
-  <a href="#" class="active">Panel</a>
-  <a href="#">Pagina</a>
-  <a href="#">Registro</a>
-  <a href="#">Visitas</a>
-  <a href="#">Redes sociales</a>
-  <a href="#">Correo</a>
-  <a href="#">Conf. general</a>
-  <a href="#">Link 8</a>
-  <a href="#">Link 9</a>
-  <a href="#">Link 10</a>
+  <a class="active" href="#"><img src="svg/dashboard.svg" alt="..." width="30"> Panel</a>
+  <a href="#"><img src="svg/browser.svg" alt="..." width="30"> Perfil</span></a>
+<a href="#popup"class="popup-link"><img src="svg/organization.svg" alt="..." width="30"> Redes sociales</a>
+  
+  <a href="#"><img src="svg/gear.svg" alt="..." width="30"> Configuracion gral.</a>
+  <a href="#"><img src="svg/comment.svg" alt="..." width="30">Enviar comentarios</a>
+<a href="#"><img src="svg/report.svg" alt="..." width="30"> Reportar un problema</a>
+  <a href="#"><img src="svg/gear.svg" alt="..." width="30"></a>
+    <a href="#"><img src="svg/gear.svg" alt="..." width="30">Link 8</a>
+  <a href="#"><img src="svg/gear.svg" alt="..." width="30">Link 9</a>
+  <a href="#"><img src="svg/gear.svg" alt="..." width="30">Link 10</a>
 </div>
 
 <a href="#"><input type='button' name='boton' value='Suscribete' class="btn btn-success" style='width:310px; height:30px'>
+
+<div class="modal-wrapper" id="popup">
+    <div class="popup-contenedor">
+      <h2>Redes sociales:</h2>
+      <p><body>
+  <div>
+    <a href="https://www.facebook.com/Proyecto-de-Vida-y-Amor-AC-494334077966928" class="facebook"><i class="fa fa-facebook"style="font-size: 50px"></i> Facebook</a> 
+    <a href="https://twitter.com/ProyectodeVid16" class="twitter"><i class="fa fa-twitter" style="font-size: 50px"></i> Twitter</a> 
+    <a href="https://www.instagram.com/pvidayamor/" class="instagram"><i class="fa fa-instagram"style="font-size: 50px"></i> Instagram</a>
+    <a href="https://www.youtube.com/channel/UCNUvRV5245sHvDQFe61U5jQ/featured?view_as=subscriber" class="youtube"><i class="fa fa-youtube" style="font-size: 50px"></i> Youtube</a> </div></body></p>
+      <a class="popup-cerrar" href="#">X</a></div></div></a>
 
 </body>
 </html>
