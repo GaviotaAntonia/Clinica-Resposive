7b26feefcde466b0d4b9b05634bf9cf14333da62
GaviotaAntonia <lunita_hermoz@hotmail.com>
2019-07-26 10:02:11 -0500

    cambios de logueo exitoso

--- conexion/logueo.php
+++ conexion/logueo.php
@@ -5,12 +5,16 @@
 
 		$usuario=$_POST['usuario'];
 		$contra=$_POST['pswd'];
-		$q="SELECT COUNT(*) as contar FROM usuario where usuario='$usuario' and contra='$contra'";
+
+		$q="SELECT COUNT(*) as contar FROM usuario where usuario='$usuario' and contrasenna='$contra'";
 		$result=mysqli_query($conexion,$q);
-		$que=mysqli_fetch(result);
+		$que=mysqli_fetch_array($result);
 		if ($que['contar']>0) {
 			$_SESSION['usuario']=$usuario;
-			header("prueba.php");
+			header("conexion/prueba.php");
+			echo "Hola has entrado como administrador";
+				echo "<h1>Bienvenido $usuario </h1>";
+
 		}else
 		{
 		echo "DATOS INCORRECTOS";
