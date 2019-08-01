1661801178a2eb2ab30005751f5336ce44e1839c
GaviotaAntonia <lunita_hermoz@hotmail.com>
2019-08-01 17:04:07 -0500

    nuevos cambios

---
+++ formusuario.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <title>Inicio/Contacto</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <link rel="shortcut icon" type="image/png" href="imagenes/logotiposinfondo.png"> <!--FAVICON-->
+ <!-- fuentes utilizadas  -->
+ 
+  <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+  <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">  <!-- termino de fuentes utilizadas-->
+</head>
+<body>
+
+  <!--Inicio Footer-->
+  <?php include 'nav3contact.html'; ?>
+<!--Final Foooter-->
+
+ <style>
+.vertical-menu {
+  width: 310px;
+  height: 460px;
+  overflow-y: auto;
+}
+
+.vertical-menu a {
+  background-color: #A3E4D7;
+  color: black;
+  display: block;
+  padding: 12px;
+  text-decoration: none;
+}
+
+.vertical-menu a:hover {
+  background-color: #48C9B0;
+}
+
+.vertical-menu a.active {
+  background-color: #0E6251;
+  color: white;
+}
+</style>
+
+<style >
+  #popup {
+    visibility: hidden;
+    opacity: 0;
+    margin-top: 0px;
+  }
+  #popup:target {
+    visibility:visible;
+    opacity: 1;
+    background-color: rgba(0,0,0,0.8);
+    position: fixed;
+    top:0;
+    left:0;
+    right:0;
+    bottom:0;
+    margin:0;
+    z-index: 000;
+    -webkit-transition:all 1s;
+    -moz-transition:all 1s;
+    transition:all 1s;
+  }
+  .popup-contenedor {
+    position: relative;
+    margin:7% auto;
+    padding:10px 30px;
+    background-color: #fafafa;
+    color:#333;
+    border-radius: 3px;
+    width:50%;
+  }
+  a.popup-cerrar {
+    position: absolute;
+    top:3px;
+    right:3px;
+    background-color: #333;
+    padding:7px 10px;
+    font-size: 20px;
+    text-decoration: none;
+    line-height: 1;
+    color:#fff;
+  }
+
+  a.popup-link {
+      text-align: justify;
+  }
+</style>
+</head>
+<body>
+  <div class="row">
+    <div class="col-sm-3">
+
+ 
+      </div>
+      <div class="col-sm-8">
+
+      </div>
+    </div>
+<div class="modal-wrapper" id="popup">
+    <div class="popup-contenedor">
+      <h2>Redes sociales:</h2>
+        <div>
+          <a href="https://www.facebook.com/Proyecto-de-Vida-y-Amor-AC-494334077966928" class="facebook"><i class="fa fa-facebook"style="font-size: 50px"></i> Facebook</a>
+          <a href="https://twitter.com/ProyectodeVid16" class="twitter"><i class="fa fa-twitter" style="font-size: 50px"></i> Twitter</a>
+          <a href="https://www.instagram.com/pvidayamor/" class="instagram"><i class="fa fa-instagram"style="font-size: 50px"></i> Instagram</a>
+          <a href="https://www.youtube.com/channel/UCNUvRV5245sHvDQFe61U5jQ/featured?view_as=subscriber" class="youtube"><i class="fa fa-youtube" style="font-size: 50px"></i> Youtube</a>
+          <a href="" class="" style="background-color: black"><i class="fa fa-users"></i></a>
+        </div>
+          <a class="popup-cerrar" href="#">X</a>
+    </div>
+</div>
+
+
+  <!--Inicio Footer-->
+  
+  <?php include 'footerusario.html; ?>
+<!--Final Foooter-->
+</body>
+
+</html>
